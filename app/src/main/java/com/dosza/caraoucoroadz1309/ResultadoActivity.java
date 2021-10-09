bb2bdb9c1d4242bddedc707c26aa83b731695cff
dosza <oliveira.daniel109@gmail.com>
2021-10-08 21:23:30 -0300

    add textView resultado

--- app/src/main/java/com/dosza/caraoucoroadz1309/ResultadoActivity.java
+++ app/src/main/java/com/dosza/caraoucoroadz1309/ResultadoActivity.java
@@ -2,15 +2,22 @@ package com.dosza.caraoucoroadz1309;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ResultadoActivity extends AppCompatActivity {
+
     private ImageView image;
     private Button botaoVoltar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +27,10 @@ public class ResultadoActivity extends AppCompatActivity {
         Bundle dados = getIntent().getExtras();
         int flag = dados.getInt("numero");
         int escolhaUsuario = dados.getInt("usuarioescolha");
-       // Toast.makeText(getApplicationContext(),Integer.toString(flag),Toast.LENGTH_SHORT).show();
-        image = findViewById(R.id.imageResultado);
+        TextView textResult = findViewById(R.id.textResult);
+        image = (ImageView) findViewById(R.id.imageResultado);
+
+
         botaoVoltar = findViewById(R.id.botaoVoltar);
 
         botaoVoltar.setOnClickListener(new View.OnClickListener() {
@@ -30,15 +39,16 @@ public class ResultadoActivity extends AppCompatActivity {
                finish();
             }
         });
+
         if ( flag == 0){
             image.setImageResource(R.drawable.moeda_cara);
         }else{
             image.setImageResource(R.drawable.moeda_coroa);
         }
         if ( flag == escolhaUsuario){
-            Toast.makeText(getApplicationContext(),"Você venceu !!",Toast.LENGTH_LONG).show();
+            textResult.setText("Você venceu!!");
         }else{
-            Toast.makeText(getApplicationContext(),"Você perdeu !!",Toast.LENGTH_LONG).show();
+            textResult.setText("Você perdeu!!");
         }
     }
 }
